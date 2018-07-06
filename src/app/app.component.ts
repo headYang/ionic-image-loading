736ced7c8b1dc3fd25c56b03c9e7fc482e65804c
Jin Yang <yang.youth1020@gmail.com>
2018-07-07 00:13:21 +0800

    Add Image-loader-package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,22 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { ImageLoaderConfig } from 'ionic-image-loader';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+  constructor(platform: Platform,
+     statusBar: StatusBar,
+     splashScreen: SplashScreen,
+     private imageloaderConfig: ImageLoaderConfig) {
+      platform.ready().then(() => {
+      this.imageloaderConfig.enableFallbackAsPlaceholder(true);
+      this.imageloaderConfig.setFallbackUrl('assets/imgs/logo.png');
+      this.imageloaderConfig.setMaximumCacheAge(24 * 60 * 60 * 1000);  
       statusBar.styleDefault();
       splashScreen.hide();
     });
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { ImageLoader } from 'ionic-image-loader';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  users = [];
+  jsonData = null;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: HttpClient, private imgloader: ImageLoader) {
 
+  }
+
+  loadData() {
+    if(!this.jsonData) {
+      this.http.get('https://randomuser.me/api/?results=100')
+          .subscribe(res => {
+            this.users = res['results'];
+            this.jsonData = res['results'];
+          });
+    } else {
+      this.users = [];
+      setTimeout(() => {
+        this.users = this.jsonData;
+      }, 1000);
+    }
+  }
+
+  clearCache(ev) {
+    this.imgloader.clearCache();
+    ev.complete(); 
+  }
+
+  onImageLoad(event) {
+    console.log('image ready!');
   }
 
 }
